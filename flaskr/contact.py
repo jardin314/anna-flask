1ba5fd5df3980fee975d8067c336dcc828e9ca72
Jordan Coe <jardinc97@gmail.com>
2023-12-24 16:23:36 -0500

    Configure the back end to store contact info when received from (yet-unwritten) html form

---
+++ flaskr/contact.py
@@ -0,0 +1,37 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from flaskr.db import get_db
+
+bp = Blueprint('contact', __name__, url_prefix='/contact')
+
+@bp.route('/contact', methods=('GET', 'POST'))
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        messageBody = request.form['body']
+        db = get_db()
+        error = None
+
+        if not name:
+            error = 'Name is required'
+        elif not email:
+            error = 'Email is required'
+
+        if error is None:
+            try:
+                db.execute(
+                        "INSERT INTO contactInfo (name, email, phone, messageBody) VALUES (?, ?, ?, ?)",
+                        (name, email, phone, messageBody)
+                        )
+                db.commit()
+            except:
+                error = 'Something went wrong!'
+            else:
+                # TODO add to this
+                return redirect(url_for(''))
